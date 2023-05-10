225ffbf714c65be3bf77ef05da0c5288e8dbe58b
Plastic Digits <plasticdigits@protonmail.com>
2023-05-10 05:57:26 -0700

    Update telegram link

--- constants/social.js
+++ constants/social.js
@@ -1,5 +1,5 @@
 export const SOCIAL_TWITTER = "https://twitter.com/AtomicWealthAW";
-export const SOCIAL_TELEGRAM = "https://t.me/AtomicWealthportal";
+export const SOCIAL_TELEGRAM = "https://t.me/AtomicWealth";
 export const SOCIAL_GITHUB = "https://github.com/chinese-zodiac";
 
 export const LINK_TERMS_OF_USE = "https://czodiac.gitbook.io/czodiac-litepaper/terms-of-use";
